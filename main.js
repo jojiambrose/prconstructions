ef7c0a1bfe71aad90714677fbda7bb9cc9f4d466
shalomgeorge97 <shalomgeorge97@gmail.com>
2018-11-21 18:57:38 +0530

    21/11

--- main.js
+++ main.js
@@ -28,4 +28,19 @@ $(document).ready(function() {
       );
     } // End if
   });
+  var selectedClass = "";
+  $(".filter").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div")
+      .not("." + selectedClass)
+      .fadeOut()
+      .removeClass("animation");
+    setTimeout(function() {
+      $("." + selectedClass)
+        .fadeIn()
+        .addClass("animation");
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
 });
