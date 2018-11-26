f388224f9ca47c2c64db463f6d24cf2702ad7659
shalomgeorge97 <shalomgeorge97@gmail.com>
2018-11-26 15:47:53 +0530

    minor changes

--- main.js
+++ main.js
@@ -3,31 +3,34 @@ $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
 
   // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on("click", function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      //toggle back nav on selection
-      $("#myNavbar").removeClass("in");
+  $(".navbar a,a[href='#about'], footer a[href='#myPage']").on(
+    "click",
+    function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        //toggle back nav on selection
+        $("#myNavbar").removeClass("in");
 
-      // Store hash
-      var hash = this.hash;
+        // Store hash
+        var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        900,
-        function() {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top
+          },
+          900,
+          function() {
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          }
+        );
+      } // End if
+    }
+  );
   var selectedClass = "";
   $(".filter").click(function() {
     selectedClass = $(this).attr("data-rel");
