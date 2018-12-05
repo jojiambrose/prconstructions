a143367594b4d5dfd5fcebc51cba0e5c34f4207d
shalomgeorge97 <shalomgeorge97@gmail.com>
2018-12-05 16:11:04 +0530

    ui

--- contactform.php
+++ contactform.php
@@ -4,11 +4,11 @@ if(isset($_POST['submit'])){
     $name= $_POST['name'];
     $email= $_POST['email'];
     $message= $_POST['message'];
-
+     echo('test');
     $mailTo = "shalomgeorge97@gmail.com";
     $header ="From: ".$name; 
     $txt ="You have recieved a message from".$name.".\n\n".$message;
 
     mail($mailTo, $txt, $header);
     header("Location: index.php?mailsend")
-}
+ }
